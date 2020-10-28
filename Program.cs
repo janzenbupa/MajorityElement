3033ad4ff724065e1ddffb9d29031512e8b25835
janzenbupa <62074171+janzenbupa@users.noreply.github.com>
2020-10-28 13:59:58 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RomanToInt
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(majorElement(new int[] { 2, 2, 1, 1, 1, 2, 2 }));
+        }
+
+        static int majorElement(int[] nums)
+        {
+            var x = nums.GroupBy(i => i).OrderByDescending(i => i.Count()).Select(i => i.Key);
+
+            return x.ElementAt(0);
+        }
+    }
+}
